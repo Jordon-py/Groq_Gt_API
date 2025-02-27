6e733035e3cdcc5227d422714944fbd5040cf819
Christopher <c.jordon@icloud.com>
2025-02-27 08:47:40 -0800

    new

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ import os
 from dotenv import load_dotenv
 
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 # ---------------------------------> Load API key from .env file
 load_dotenv('.env')
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
@@ -20,8 +33,7 @@ def home():
 
 
 # -------------------------- Runs API
-if __name__ == "__main__":
-    import os
+
     port = int(os.environ.get("PORT", 5000))  # Use Heroku's port
     app.run(debug=True, host="0.0.0.0", port=port)
 
